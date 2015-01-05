ea656331e2f462d50f29b91f800fabe607aaf0d3
yuichiro <yuichiro.utsumi@gmail.com>
2015-01-05 23:49:34 +0900

    let, let*, letrecをやった。

--- section7/test.scm
+++ section7/test.scm
@@ -26,3 +26,21 @@
 (define (numbers-only walker)
     (lambda (proc lis)
         (walker proc (filter number? lis))))
+
+;; ローカルな再帰手続きをletrecで書く
+(letrec ((sum (lambda (lis)
+                  (cond [(null? lis) 0]
+                        [(number? (car lis)) (+ (car lis) (sum (cdr lis)))]
+                        [else (sum (cdr lis))]))))
+    (sum '(1 3 #f 6 #t 9)))
+
+;; ローカルな相互再帰手続きをletrecで書く
+(letrec ((even? (lambda (n)
+                    (cond [(= n 0) #t]
+                          [(> n 0) (odd? (- n 1))]
+                          [else (odd? (+ n 1))])))
+         (odd?  (lambda (n)
+                    (cond [(= n 0) #f]
+                          [(> n 0) (even? (- n 1))]
+                          [else (even? (+ n 1))]))))
+    (even? 1280))
